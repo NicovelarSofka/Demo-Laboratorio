29d75c0b7256f52316f58478827d1334d5654c48
nicovelar <nicovelar@gmail.com>
2022-12-27 14:24:59 -0300

    First commit

---
+++ Frontend/Aplicación/ClientApp/app/components/persona/persona.component.ts
@@ -0,0 +1,11 @@
+import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'fetchdata',
+    templateUrl: './fetchdata.component.html'
+})
+export class PersonaComponent {
+ 
+}
+
