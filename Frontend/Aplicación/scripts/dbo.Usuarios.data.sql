2bc8a3ccc749ae8cca1a2e42ea8aa17a69f9f97d
NicovelarSofka <nicovelar@gmail.com>
2023-01-03 13:11:43 -0300

    db datos

---
+++ Frontend/Aplicación/scripts/dbo.Usuarios.data.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO [dbo].[Usuarios] ( [Identificacion], [PrimerNombre], [SegundoNombre], [PrimerApellido], [SegundoApellido], 
+[Telefono], [Direccion], [Correo], [FechaNacimiento]) VALUES 
+( 'MarMor', 'Marcos', NULL, 'Morillas', NULL, '097874678', 'Calle Toledo', 'marquitos@gmail.com', 1970-01-10),
+( 'EnrPaz', 'Enrique', NULL, 'Paz', NULL, '093678543', 'Avenida Francia', 'enriquepaz@gmail.com', 1995-05-23),
+( 'PauRam', 'Paula', NULL, 'Ramos', NULL, '095840175', 'Calle Don Alberto', 'paulitaramos@gmail.com', 2000-10-19),
+( 'PabGon', 'Pablo', NULL, 'Gonzales', NULL, '092487245', 'Edificio Santa Rosa', 'elpablo@gmail.com', 1987-03-05),
+( 'SofMor', 'Sofia', NULL, 'Morales', NULL, '093847162', 'Esquina Don Pascual', 'sofimorales@gmail.com', 2005-06-13)
+
+
+
+
---
+++ Frontend/Aplicación/scripts/dbo.Usuarios.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Usuarios] (
+    [Id]              INT           IDENTITY (1, 1) NOT NULL,
+    [Identificacion]  VARCHAR (15)  NOT NULL,
+    [PrimerNombre]    NVARCHAR (50) NOT NULL,
+    [SegundoNombre]   NVARCHAR (50) NULL,
+    [PrimerApellido]  NVARCHAR (50) NOT NULL,
+    [SegundoApellido] NVARCHAR (50) NULL,
+    [Telefono]        NVARCHAR (50) NOT NULL,
+    [Direccion]       NVARCHAR (50) NOT NULL,
+    [Correo]          NVARCHAR (50) NOT NULL,
+    [FechaNacimiento] DATETIME      NOT NULL,
+    CONSTRAINT [PK_Usuarios] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
