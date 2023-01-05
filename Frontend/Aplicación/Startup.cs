803524c42c14da5c93653e9144a0897bd3a882a4
ariel.sierra <ariel.sierra@PSOFKA0920>
2023-01-05 09:03:26 -0500

    fix code

--- Frontend/Aplicación/Startup.cs
+++ Frontend/Aplicación/Startup.cs
@@ -23,6 +23,14 @@ namespace Aplicación
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: "policy",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin();
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +47,7 @@ namespace Aplicación
             }
 
             app.UseStaticFiles();
+            app.UseMigrationsEndPoint
 
             app.UseMvc(routes =>
             {
